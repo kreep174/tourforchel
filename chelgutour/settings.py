08d33621ed00f4e73504aaa8194f65db9dcf4347
kreep174 <42908598+kreep174@users.noreply.github.com>
2020-04-03 11:20:10 +0500

    Update settings.py

--- chelgutour/settings.py
+++ chelgutour/settings.py
@@ -125,7 +125,3 @@ STATICFILES_DIRS = [STATIC_DIR]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-# Heroku: Update database configuration from $DATABASE_URL.
-import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
