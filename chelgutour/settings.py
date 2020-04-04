4dbe9e62bf3f4c5fff157d6471f259bc9821af94
kreep174 <42908598+kreep174@users.noreply.github.com>
2020-04-04 15:05:45 +0500

    Update settings.py

--- chelgutour/settings.py
+++ chelgutour/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'w94t43@1c5ru7f8kxok3dfpyhwt4a*
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
 DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [warm-garden-01424.herokuapp.com]
 
 
 # Application definition
